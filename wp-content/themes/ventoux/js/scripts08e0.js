abaa6ac4220f73b02daf6cee1530a93078291be1
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2017-02-08 05:27:25 +0530

    images height modified. Navbar active. achievements changed

--- wp-content/themes/ventoux/js/scripts08e0.js
+++ wp-content/themes/ventoux/js/scripts08e0.js
@@ -44,11 +44,11 @@ $(function () // on document.ready()
           header = document.querySelector("#fixed");
             if (distanceY > shrinkOn) {
               classie.add(header,"smaller");
-              $(".span-underline").removeClass("topnav-span");
+              $(".index-page-nav .span-underline").removeClass("topnav-span");
             } else {
             if (classie.has(header,"smaller")) {
               classie.remove(header,"smaller");
-               $(".span-underline").addClass("topnav-span");
+               $(".index-page-nav .span-underline").addClass("topnav-span");
             }
           }
       });
